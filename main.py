80b814b4398512fc3d5ff150bf98b47c4c4a3f18
gmatiascr <gmatiascr62@gmail.com>
2025-01-02 22:44:03 -0300

    Commit automático

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
